cec90e8844504a9c13e0af9881131e70297a1594
Kushagra Agrawal <kush4409@gmail.com>
2024-07-13 13:12:36 +0530

    Create resume.vue

---
+++ pages/resume.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+definePageMeta({
+  documentDriven: false
+})
+
+const title = 'Resume - Kushagra Agrawal'
+const description = 'View or download the resume of Kushagra Agrawal.'
+
+useSeoMeta({
+  title,
+  description,
+})
+
+defineOgImageComponent('General')
+
+// Redirect to Google Drive link
+onMounted(() => {
+  window.location.href = 'https://drive.google.com/file/d/1QEgKlGB0nvWx3KZVkkibFPhVRcQiz-2m';
+});
+</script>
+
+<template>
+  <div>
+    <AppSection>
+      <ParagraphDecoration />
+      <AppParagraph look="heading" class="mt-4" tag="h1">Redirecting to my Resume...</AppParagraph>
+      <p>If you are not redirected automatically, <a href="https://drive.google.com/file/d/1QEgKlGB0nvWx3KZVkkibFPhVRcQiz-2m">click here</a>.</p>
+    </AppSection>
+  </div>
+</template>
+
+<style scoped>
+/* Add any styles you need here */
+</style>
